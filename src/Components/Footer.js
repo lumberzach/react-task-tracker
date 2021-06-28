61b6c53b3dcbee4f14970f2f329b4828a59ff184
lumberzach <boxcarracer0110@gmail.com>
2021-06-28 02:40:26 -0700

    Camping list react app

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p>Copyright &copy; 2021</p>
+            <Link to="/">Home {' '}</Link>
+            <Link to="/about">About</Link>
+            
+        </footer>
+    )
+}
+
+export default Footer
